95c607f1afb8cab21f3dafacbc2445d789b21d98
Divy Mav <divymav5@gmail.com>
2024-04-24 12:13:34 +0530

    Correcting some thing

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -29,7 +29,7 @@ function SamplePrevArrow(props) {
 function VideoText() {
     return (
         <div className='video-text'>
-            <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" width="60%" height="auto" controls="controls" autoplay="true" />
+            <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" width="60%" height="auto" controls="controls" autoPlay />
             <p className='carousel-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of </p>
         </div>
     )
